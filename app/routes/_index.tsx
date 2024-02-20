adb53f9966c8dd590aec41921110c50e2ad34c38
Ashton Christensen <armadillomaster@gmail.com>
2024-02-19 22:48:23 -0800

    Add Tailwind CSS and update page title and description

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -2,15 +2,18 @@ import type { MetaFunction } from '@remix-run/node';
 
 export const meta: MetaFunction = () => {
   return [
-    { title: 'New Remix App' },
-    { name: 'description', content: 'Welcome to Remix!' },
+    { title: 'Garden Log' },
+    {
+      name: 'description',
+      content: 'A comprehensive app for taking care of your garden.',
+    },
   ];
 };
 
 export default function Index() {
   return (
-    <div
-      style={{ fontFamily: 'system-ui, sans-serif', lineHeight: '1.8' }}
-    ></div>
+    <div style={{ fontFamily: 'system-ui, sans-serif', lineHeight: '1.8' }}>
+      <h1 className=''>Hello world!</h1>
+    </div>
   );
 }
